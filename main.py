8b4de8c2825489de02317e7b839219ff31765813
krishan <ykrihsan318@icloud.com>
2024-06-22 08:42:16 +0900

    add titles in main & change photo

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import streamlit as st
+import pandas
 
-
-st.set_page_config(layout="wide")
+st.set_page_config()
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.jpeg")
 
 with col2:
-    st.title("Krishan Sulce")
+    st.title("Krishan")
     content = """
     Hi, I am Krishan! I am a Python programmer. I graduated in 2018 with a B.tech in E.I.C.
 I have worked in Japan. 
@@ -19,3 +19,14 @@ content2 = """
 Below you can find some of the apps i have built in python . feel free to contact me!
 """
 st.write(content2)
+
+col3,col4 = st.columns(2)
+df = pandas.read_csv("data.csv",sep=";")
+
+with col3:
+    for index ,row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index ,row in df[10:].iterrows():
+        st.header(row["title"])
