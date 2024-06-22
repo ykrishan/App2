ea41e83342a7e95b5bc19c7492e77901fa86c435
krishan <ykrihsan318@icloud.com>
2024-06-22 19:34:20 +0900

    add image and source code in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas
 
-st.set_page_config()
+st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
@@ -11,7 +11,7 @@ with col1:
 with col2:
     st.title("Krishan")
     content = """
-    Hi, I am Krishan! I am a Python programmer. I graduated in 2018 with a B.tech in E.I.C.
+    Hi, I am Krishan! I am a Python programmer. I graduated in 2018 with  B.tech in E.I.C.
 I have worked in Japan. 
     """
     st.info(content)
@@ -20,13 +20,19 @@ Below you can find some of the apps i have built in python . feel free to contac
 """
 st.write(content2)
 
-col3,col4 = st.columns(2)
-df = pandas.read_csv("data.csv",sep=";")
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
-    for index ,row in df[:10].iterrows():
+    for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source code ]({row['url']}]")
 
 with col4:
-    for index ,row in df[10:].iterrows():
+    for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source code ]({row['url']}]")
